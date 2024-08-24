cf7d50d57389847cf546ab63833f119979b717a4
Master-Bw3 <bw1jochman@gmail.com>
2024-08-24 17:49:25 -0500

    add OpAddChild

---
+++ Fabric/src/main/kotlin/mod/master_bw3/hexui/fabric/casting/actions/OpAddChild.kt
@@ -0,0 +1,22 @@
+package mod.master_bw3.hexui.fabric.casting.actions
+
+import at.petrak.hexcasting.api.casting.castables.ConstMediaAction
+import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
+import at.petrak.hexcasting.api.casting.getList
+import at.petrak.hexcasting.api.casting.iota.Iota
+import at.petrak.hexcasting.api.casting.mishaps.MishapInvalidIota
+import mod.master_bw3.hexui.fabric.api.casting.getComponent
+import mod.master_bw3.hexui.fabric.api.casting.iota.ComponentIota
+import mod.master_bw3.hexui.fabric.api.componentBuilder.HasChildren
+
+object OpAddChild : ConstMediaAction {
+
+    override val argc: Int = 2
+
+    override fun execute(args: List<Iota>, env: CastingEnvironment): List<Iota> {
+        val component = args.getComponent(HasChildren::class, 0, argc)
+        val child = args.getComponent(1, argc)
+
+        return listOf(ComponentIota(component.addChild(child)))
+    }
+}
--- Fabric/src/main/kotlin/mod/master_bw3/hexui/fabric/registry/FabricHexUIActions.kt
+++ Fabric/src/main/kotlin/mod/master_bw3/hexui/fabric/registry/FabricHexUIActions.kt
@@ -6,31 +6,34 @@ import at.petrak.hexcasting.api.casting.math.HexDir
 import at.petrak.hexcasting.api.casting.math.HexPattern
 import at.petrak.hexcasting.common.lib.HexRegistries
 import at.petrak.hexcasting.common.lib.hex.HexActions
+import mod.master_bw3.hexui.fabric.casting.actions.*
 import mod.master_bw3.hexui.fabric.componentBuilder.container.LayoutComponentBuilder
-import mod.master_bw3.hexui.fabric.casting.actions.OpDisplayScreen
-import mod.master_bw3.hexui.fabric.casting.actions.OpMakeButton
-import mod.master_bw3.hexui.fabric.casting.actions.OpMakeLayout
-import mod.master_bw3.hexui.fabric.casting.actions.OpSetChildren
 import mod.master_bw3.hexui.registry.HexUIRegistrar
 
 object FabricHexUIActions : HexUIRegistrar<ActionRegistryEntry>(HexRegistries.ACTION, { HexActions.REGISTRY }) {
 
     @JvmField
-    val MAKE_BUTTON = make("component.make_button", HexDir.NORTH_EAST, "wdewd", OpMakeButton)
+    val MAKE_BUTTON = make("component.make_button", HexDir.EAST, "wdewd", OpMakeButton)
 
     @JvmField
     val MAKE_LAYOUT_VERTICAL = make("component.make_layout_vertical", HexDir.NORTH_EAST, "eewdwwdew", OpMakeLayout(
         LayoutComponentBuilder.Algorithm.VERTICAL))
 
     @JvmField
-    val MAKE_LAYOUT_HORIZONTAL = make("component.make_layout_horizontal", HexDir.NORTH_EAST, "eeewdqdee", OpMakeLayout(
+    val MAKE_LAYOUT_HORIZONTAL = make("component.make_layout_horizontal", HexDir.EAST, "eeewdqdee", OpMakeLayout(
         LayoutComponentBuilder.Algorithm.HORIZONTAL))
 
     @JvmField
-    val SET_CHILDREN = make("component.set_children", HexDir.NORTH_EAST, "deedadd", OpSetChildren)
+    val SET_CHILDREN = make("component.set_children", HexDir.EAST, "deedadd", OpSetChildren)
 
     @JvmField
-    val DISPLAY_SCREEN = make("display_screen", HexDir.NORTH_EAST, "eeeeeweee", OpDisplayScreen)
+    val ADD_CHILD = make("component.add_child", HexDir.WEST, "aqqadaa", OpAddChild)
+
+    @JvmField
+    val SET_GAP = make("component.set_gap", HexDir.EAST, "eeedqqaeedqd", OpSetGap)
+
+    @JvmField
+    val DISPLAY_SCREEN = make("display_screen", HexDir.EAST, "eeeeeweee", OpDisplayScreen)
 
     private fun make(name: String, startDir: HexDir, signature: String, action: Action) =
         make(name, startDir, signature) { action }
